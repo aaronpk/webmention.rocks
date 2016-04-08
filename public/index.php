ce4ad5d368d48c824d4764faa48a3db323aad6a8
Aaron Parecki <aaron@parecki.com>
2016-04-08 16:58:18 -0700

    splash page

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+  <title>Webmention Rocks!</title>
+  <style type="text/css">
+  img {
+    width: 100vw;
+  }
+  </style>
+</head>
+<body>
+  <img src="/assets/webmention-rocks.svg">
+</body>
+</html>

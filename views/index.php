0d9211a2df1cc72b7690a6256a9e6900da3b27af
Benjamin Goering <phoghawk@gmail.com>
2016-04-12 10:17:41 -0700

    Link to webmention spec

--- views/index.php
+++ views/index.php
@@ -5,7 +5,7 @@
 
   <section class="content">
     <h3>About this site</h3>
-    <p><b><i>Webmention Rocks!</i></b> is a validator to help you test your Webmention
+    <p><b><i>Webmention Rocks!</i></b> is a validator to help you test your <a href="https://www.w3.org/TR/webmention/">Webmention</a>
       implementation. Several kinds of tests are available on the site.</p>
   </section>
 

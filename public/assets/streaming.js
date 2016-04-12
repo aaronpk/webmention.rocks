f042e116577d5a39f273928bf80b104139dd0ebd
Aaron Parecki <aaron@parecki.com>
2016-04-12 16:42:15 +0200

    check for eventsource

--- public/assets/streaming.js
+++ public/assets/streaming.js
@@ -1,31 +1,33 @@
 $(function(){
 
-  // Subscribe to the streaming channel and insert responses as they come in
-  var socket = new EventSource('/streaming/sub?id=test-'+$("#test-num").data("num"));
+  if(window.EventSource) {
+    // Subscribe to the streaming channel and insert responses as they come in
+    var socket = new EventSource('/streaming/sub?id=test-'+$("#test-num").data("num"));
 
-  socket.onopen = function() {
-  };
+    socket.onopen = function() {
+    };
 
-  socket.onmessage = function(event) {
-    var data = JSON.parse(event.data);
-    console.log(data);
-    if(data.text.action == 'delete') {
-      $("li[data-response-id="+data.text.hash+"]").remove();
-    } else {
-      if($("li[data-response-id="+data.text.hash+"]").length == 0) {
-        $(".stream."+data.text.type).prepend(data.text.html);
+    socket.onmessage = function(event) {
+      var data = JSON.parse(event.data);
+      console.log(data);
+      if(data.text.action == 'delete') {
+        $("li[data-response-id="+data.text.hash+"]").remove();
       } else {
-        $("li[data-response-id="+data.text.hash+"]").html(data.text.html);
+        if($("li[data-response-id="+data.text.hash+"]").length == 0) {
+          $(".stream."+data.text.type).prepend(data.text.html);
+        } else {
+          $("li[data-response-id="+data.text.hash+"]").html(data.text.html);
+        }
       }
-    }
-  };
+    };
 
-  socket.addEventListener('update', function(event) {
-    // log('UPDATE(' + event.lastEventId + '): ' + event.data);
-  });
+    socket.addEventListener('update', function(event) {
+      // log('UPDATE(' + event.lastEventId + '): ' + event.data);
+    });
 
-  socket.onerror = function(event) {
-    console.log("error: ", event);
-  };
-
+    socket.onerror = function(event) {
+      console.log("error: ", event);
+    };
+  }
+  
 });
